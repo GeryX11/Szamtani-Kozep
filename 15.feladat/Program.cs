27feef1c34d844c7a1543dac42ff72ed6391e7ab
tanulo <tanulo@TANULOGEP-406>
2026-09-22 11:45:19 +0200

    Add project files.

---
+++ 15.feladat/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15.feladat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Add meg az első számot: ");
+            int szam1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Add meg az második számot: ");
+            int szam2 = int.Parse(Console.ReadLine());
+
+            double kozep = (double)(szam1 + szam2) / 2;
+            Console.WriteLine($"A számtani kőzép: {kozep}");
+            Console.ReadKey();
+        }
+    }
+}
